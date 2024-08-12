564efa536195d38cd2772fa5e97fe0dc88e1d807
Frank Kong <frkong@redhat.com>
2024-08-12 11:33:52 -0400

    chore: improve image quality and add mirroring toggle
    Signed-off-by: Frank Kong <frkong@redhat.com>

--- app/camera/camera.tsx
+++ app/camera/camera.tsx
@@ -6,11 +6,15 @@ export const WebcamCapture = () => {
   const webcamRef = useRef<Webcam>(null);
   const [imageSrc, setImageSrc] = useState<string | null>();
   const [facingMode, setFacingMode] = useState<string>("user");
+  const [mirrored, setMirrored] = useState<boolean>(false);
 
   const toggleFacingMode = () => {
     setFacingMode((prevFacingMode) =>
       prevFacingMode === "user" ? "environment" : "user",
     );
+  };
+  const toggleMirrored = () => {
+    setMirrored((prevMirrored) => !prevMirrored);
   };
 
   const capture = () => {
@@ -24,8 +28,10 @@ export const WebcamCapture = () => {
       <Webcam
         audio={false}
         ref={webcamRef}
-        screenshotFormat="image/jpeg"
+        screenshotFormat="image/png"
+        mirrored={mirrored}
         videoConstraints={{ facingMode }}
+        screenshotQuality={1}
       />
       <button
         onClick={capture}
@@ -39,11 +45,17 @@ export const WebcamCapture = () => {
       >
         Toggle Camera
       </button>
+      <button
+        onClick={toggleMirrored}
+        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      >
+        Mirror Image
+      </button>
       <h1>Captured Image</h1>
       {imageSrc && (
         <div>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={imageSrc} alt="Captured Image" />
+          <img src={imageSrc} alt="Captured Webcam Screenshot" />
           <button
             onClick={() => setImageSrc(null)}
             className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
