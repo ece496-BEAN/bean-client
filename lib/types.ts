1a55f0b05bb50bfd47c3500457786a28d7f60372
Frank Kong <frankkong777@gmail.com>
2025-02-27 01:20:24 -0500

    chore: update common types
    Signed-off-by: Frank Kong <frankkong777@gmail.com>

--- lib/types.ts
+++ lib/types.ts
@@ -13,51 +13,68 @@ export type DefaultTransactionCategory =
   | "Entertainment"
   | "Housing";
 
-export type Transaction = {
+export type WriteOnlyTransaction = {
+  uuid?: string; // UUID not needed when creating a new transaction
+  name: string;
+  description?: string;
+  amount: number; // 2 Decimal Places
+  category_uuid: string; // UUID
+};
+
+export type ReadOnlyTransaction = {
   id: string; // UUID
   group_id: string; // UUID
   name: string;
-  description: string;
-  amount: number;
-  date: string;
+  description?: string;
+  amount: number; // 2 Decimal Places
   category: Category;
 };
+
+export type Transaction = WriteOnlyTransaction | ReadOnlyTransaction;
 
 export type Category = {
   id: string; // UUID
   name: DefaultTransactionCategory | string;
-  description: string;
+  description?: string;
   legacy: boolean;
 };
 
 export type TransactionGroup = {
-  id: string; // UUID
+  id?: string; // UUID
   name: string;
-  description: string;
-  source: string;
+  description?: string;
+  source: string | null;
   date: string;
   transactions: Transaction[];
 };
 
-export type BudgetItem = {
+export type WriteOnlyBudgetItem = {
+  uuid: string; // UUID
+  allocation: number; // 2 decimal places
+  category_uuid: string; // UUID
+};
+
+export type ReadOnlyBudgetItem = {
   id: string; // UUID
   budget_id: string; // UUID
   allocation: number; // 2 decimal places
   category: Category;
 };
 
+export type BudgetItem = WriteOnlyBudgetItem | ReadOnlyBudgetItem;
+
 export type Budget = {
   id: string; // UUID
   name: string;
-  description: string;
+  description?: string;
   start_date: string;
   end_date: string;
   items: BudgetItem[];
 };
 
-export type PaginatedServerResponse = {
+export type PaginatedServerResponse<T> = {
   count: number;
   next: string | null;
   previous: string | null;
-  results: unknown[];
+  results: T[];
 };
