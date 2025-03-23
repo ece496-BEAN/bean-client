430a7781fe4a13b8fc9d24241ed60472e3396251
Frank Kong <frankkong777@gmail.com>
2025-03-22 23:03:44 -0400

    chore: added placeholder value before adding category mapping logic
    Signed-off-by: Frank Kong <frankkong777@gmail.com>

--- app/api/plaid/transactions/route.ts
+++ app/api/plaid/transactions/route.ts
@@ -3,26 +3,17 @@ import * as plaid from "plaid";
 import { getAccessToken, plaidClient } from "../lib/plaid-server";
 import { DEV_USER_ID } from "@/lib/plaid";
 import {
+  ReadOnlyTransaction,
+  Transaction,
+  TransactionGroup,
+  WriteOnlyTransaction,
+} from "@/lib/types";
+import {
   mapPlaidToBeanTransactionCategory,
   PlaidPrimaryTransactionCategory,
 } from "@/lib/data-mapping";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-type Transaction = {
-  name: string;
-  description: string;
-  amount: number; // 2 Decimal Places
-  category: string;
-};
-
-type TransactionGroup = {
-  name: string;
-  description?: string;
-  source: string | null;
-  date: string;
-  transactions: Transaction[];
-};
 
 export async function GET(req: NextRequest) {
   try {
@@ -79,22 +70,30 @@ export async function GET(req: NextRequest) {
     console.log(added);
 
     // Convert the Plaid transaction type to our internal format
-    const processed_transactions: TransactionGroup[] = added.map(
-      (txn: plaid.Transaction) => ({
+    const processed_transactions: Omit<TransactionGroup<Transaction>, "id">[] =
+      added.map((txn: plaid.Transaction) => ({
         name: txn.name,
         description: txn.name ?? "",
-        source: "plaid",
+        source: null,
         date: txn.date,
         transactions: [
           {
             name: txn.name,
             description: txn.name,
             amount: txn.amount,
-            category: txn.personal_finance_category?.primary ?? "other",
+            // TODO(Fred): Map the Plaid category to our internal category
+            id: "", // Placeholder value (doesn't need to be set as it's ignored during POST)
+            group_id: "", // Placeholder value (doesn't need to be set as it's ignored during POST)
+            category: {
+              id: "", // Placeholder value (will be replaced during category mapping)
+              name: txn.personal_finance_category?.primary ?? "OTHER",
+              description: "",
+              is_income_type: false,
+              legacy: false,
+            },
           },
         ],
-      }),
-    );
+      }));
     return NextResponse.json({ transactions: processed_transactions });
   } catch (error) {
     console.error(error);
