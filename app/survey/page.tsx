78c741658ade3b9bff28c1c251dd86d6bf03c836
Fred Chen <chenfred9898@gmail.com>
2025-03-26 00:23:32 -0400

    fix: too many jwt providers wrapping causing duplicate stale data

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -1,16 +1,10 @@
 import SurveyPage from "@/components/survey-page";
 import CategoryProvider from "@/contexts/CategoriesContext";
-import JwtProvider from "@/app/lib/jwt-provider";
-import BudgetProvider from "@/contexts/BudgetContext";
 
 export default function Page() {
   return (
-    <JwtProvider>
-      <BudgetProvider>
-        <CategoryProvider>
-          <SurveyPage />
-        </CategoryProvider>
-      </BudgetProvider>
-    </JwtProvider>
+    <CategoryProvider>
+      <SurveyPage />
+    </CategoryProvider>
   );
 }
