2ea581945a45f59c00cad12bf41bf340c43bacfe
Fred Chen <chenfred9898@gmail.com>
2025-03-26 00:18:50 -0400

    fix: idk wrapping budget provider solves jwt

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -1,13 +1,16 @@
 import SurveyPage from "@/components/survey-page";
 import CategoryProvider from "@/contexts/CategoriesContext";
 import JwtProvider from "@/app/lib/jwt-provider";
+import BudgetProvider from "@/contexts/BudgetContext";
 
 export default function Page() {
   return (
     <JwtProvider>
-      <CategoryProvider>
-        <SurveyPage />
-      </CategoryProvider>
+      <BudgetProvider>
+        <CategoryProvider>
+          <SurveyPage />
+        </CategoryProvider>
+      </BudgetProvider>
     </JwtProvider>
   );
 }
